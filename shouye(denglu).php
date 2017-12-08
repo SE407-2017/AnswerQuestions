92da9a17cd2422759af939b2873ecb3f22aca54c
wingsOfPiano <31919561+wingsOfPiano@users.noreply.github.com>
2017-12-08 16:59:19 +0800

    Add files via upload

--- shouye(denglu).php
+++ shouye(denglu).php
@@ -3,16 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <title>首页(登录）</title>
-    <link rel="stylesheet" type="text/css" href="首页（登录）.css"><!--外部样式-->
-    <script>
-        function over(obj) {
-            obj.style.cssText="animation:toRotateOne 1s forwards";
-        }
-
-        function out(obj) {
-            obj.style.cssText="animation:toRotateTwo 1s forwards";
-        }
-    </script>
+    <link rel="stylesheet" type="text/css" href="首页（登录）.css">
+    <link rel="stylesheet" type="text/css" href="public.css"><!--外部样式-->
+    <script type="text/javascript" src="动画.js"></script>
 </head>
 <body>
     <h1>课业难题交流</h1>
@@ -35,8 +28,8 @@
                 <option value="0" >课程</option>
                 <option value="1" >问题</option>
             </select>
-			<br>
-			<select name="shaixuan" style="margin-left: 30px"><!--筛选-->
+			<br><br>
+			<select name="shaixuan" class="selection"><!--筛选-->
 			<option value="0">按提问时间升序</option>
 			<option value="1">按提问时间降序</option>
 			<option value="2">按回复数升序</option>
@@ -46,57 +39,60 @@
 			<option value="6">按点赞数升序</option>
 			<option value="7">按点赞数降序</option>
 			</select>
-            <br>
+            <br><br>
             <input type="submit" value="搜索" class="upLoad">
 	
 		</form>
     </center>
-			<br>   
-			
-			热点讨论区：	
-			<br>
-			<table style="margin-left: 30px" >
-			
-			<tr style="text-align: left">
-            <th width="600px" style="float: left;margin-left: 20px">内容</th>
-            <th width="100px">提问人</th>
-            <th width="100px">回复数目</th>
-            <th width="100px">是否解决</th>
-            <th width="200px">提问时间</th>
-			<th width="100px">点赞数</th>
-			<?php
-			$con=mysqli_connect("localhost","root","","users");
-			if (!$con){
-				die("连接失败:".mysqli_connect_error());
-			}
-			
-			$result = mysqli_query($con,"SELECT * FROM problem ORDER BY reply_num DESC");
-			$num=mysqli_num_rows($result);
-			
-			if ($num>4)
-			{
-				$number=4;
-			}
-			else
-			{
-				$number=$num;
-			}
-			
-			for ($x=1;$x<=$number;$x++)
-			{
-				$row=mysqli_fetch_array($result);
-				echo "<tr>"."<td>";
-				$text=$row["text_content"];
-				echo "<a style='margin-left: 30px' target='_blank' href='answer_question.php?content=$text'>".$text."</a>"."</td>";
-				echo "<td>".$row['people']."</td>";
-				echo "<td>".$row['reply_num']."</td>";
-				echo "<td>".$row['state']."</td>";
-				echo "<td>".$row['time']."</td>";
-				echo "<td>".$row['praise_num']."</td>";
-				echo "</tr>";	
-			}
-			
-			mysqli_close($con); 
-			?>
+
+        <br>
+        <p>热点讨论区：</p>
+        <img src="picture_dissicion.jpg">
+
+        <table>
+            <tr>
+                <th width="400px" style="float: left;" >内容：</th>
+                <th width="100px">提问人：</th>
+                <th width="100px">回复数目：</th>
+                <th width="100px">是否解决：</th>
+                <th width="100px">提问时间：</th>
+                <th width="100px">点赞数：</th>
+            </tr>
+
+            <?php
+            $con=mysqli_connect("localhost","root","","users");
+            if (!$con){
+                die("连接失败:".mysqli_connect_error());
+            }
+
+            $result = mysqli_query($con,"SELECT * FROM problem ORDER BY reply_num DESC");
+            $num=mysqli_num_rows($result);
+
+            if ($num>4)
+            {
+                $number=4;
+            }
+            else
+            {
+                $number=$num;
+            }
+
+            for ($x=1;$x<=$number;$x++)
+            {
+                $row=mysqli_fetch_array($result);
+                echo "<tr>"."<td>";
+                $text=$row["text_content"];
+                echo "<a style='margin-left: 30px' target='_blank' href='answer_question.php?content=$text'>".$text."</a>"."</td>";
+                echo "<td>".$row['people']."</td>";
+                echo "<td>".$row['reply_num']."</td>";
+                echo "<td>".$row['state']."</td>";
+                echo "<td>".$row['time']."</td>";
+                echo "<td>".$row['praise_num']."</td>";
+                echo "</tr>";
+            }
+
+            mysqli_close($con);
+            ?>
+        </table>
 </body>
 </html>
---
+++ 动画.js
@@ -0,0 +1,7 @@
+function over(obj) {
+    obj.style.cssText="animation:toRotateOne 1s forwards";
+}
+
+function out(obj) {
+    obj.style.cssText="animation:toRotateTwo 1s forwards";
+}
